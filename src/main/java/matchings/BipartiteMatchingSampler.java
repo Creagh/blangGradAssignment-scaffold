bcf00bab27af6d42829969d58eb7d6f1dcfdfdcd
Creagh Briercliffe <creagh5@hotmail.com>
2018-04-04 01:02:31 -0700

    MH sampler for bipartite matchings

--- src/main/java/matchings/BipartiteMatchingSampler.java
+++ src/main/java/matchings/BipartiteMatchingSampler.java
@@ -1,5 +1,6 @@
 package matchings;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bayonet.distributions.Multinomial;
@@ -30,6 +31,49 @@ public class BipartiteMatchingSampler implements Sampler {
   @Override
   public void execute(Random rand) {
     // Fill this. 
+	  
+	  /** 
+	   * Metropolis-Hastings Algorithm for sampling Bipartite Matchings 
+	   */
+	  
+	  List<Integer> oldMatch; // old matching
+	  double oldDens; // log density of old matching
+	  double newDens; // log density of new matching
+	  double alpha; // the acceptance probability
+	  boolean bernoulli; // a Bernoulli RV
+	  
+	  /** 
+	   * Store the old matching and calculate log density
+	   */
+	  oldMatch = new ArrayList<Integer>(matching.getConnections());
+	  oldDens = logDensity();
+	  
+	  /**
+	   *  Sample a new matching and calculate new log density.
+	   *  New matching is proposed by sampling a matching
+	   *  uniformly at random---see BipartiteMatching.xtend
+	   */
+	  matching.sampleUniform(rand);
+	  newDens = logDensity();
+	  
+	  /**
+	   *  Calculate the acceptance probability
+	   */
+	  alpha = Math.min(1, Math.exp(newDens - oldDens));
+	  
+	  /**
+	   *  Determine whether to accept the new matching
+	   *  Flip a weighted coin: heads = accept, tails = reject
+	   */
+	  bernoulli = Generators.bernoulli(rand, alpha);
+	  
+	  if(!bernoulli) {
+		  // Reject & replace all new values with the old matching
+		  for(int i=0; i < oldMatch.size(); i++) {
+			  matching.getConnections().set(i, oldMatch.get(i));
+		  }
+	  }
+	  
   }
   
   private double logDensity() {
